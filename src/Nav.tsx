63ad28717f48bd5c133bfd5663eb0ec055a4a99a
dpbaines <david.peter.baines@gmail.com>
2025-07-12 22:39:43 -0700

    Creating navbar

---
+++ src/Nav.tsx
@@ -0,0 +1,19 @@
+/**
+ * Nav bar component.
+ */
+function Nav() {
+  return (
+    <nav>
+      <ul className="nav-list">
+        <li className="nav-item">
+          <p>David's website</p>
+        </li>
+        <li className="nav-item">
+          <p>Idk</p>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
